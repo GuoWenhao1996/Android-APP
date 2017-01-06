f7a62fb92aef0ed4a6de5265b95d2787ca2036ba
GuoWenhao1996 <1842297753@qq.com>
2017-01-07 01:53:38 +0800

    上传抢红包app1.0

---
+++ MyQianghongbao/app/src/main/java/com/example/guowh/myqianghongbao/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.guowh.myqianghongbao;
+
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
+
+/*MainActivity中的代码基本没改变：*/
+public class MainActivity extends AppCompatActivity {
+    private final Intent mAccessibleIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+    private Button switchPlugin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        switchPlugin = (Button) findViewById(R.id.button_accessible);
+        updateServiceStatus();
+
+        switchPlugin.setOnClickListener(new View.OnClickListener() {//创建监听
+            @Override
+            public void onClick(View view) {
+                openAccessibilityServiceSettings();
+            }
+        });
+    }
+
+    /*开启插件的按钮*/
+    public void onButtonClicked(View view) {
+        startActivity(mAccessibleIntent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateServiceStatus();
+    }
+
+    private void updateServiceStatus() {
+        boolean serviceEnabled = false;
+        AccessibilityManager accessibilityManager = (AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
+        List<AccessibilityServiceInfo> accessibilityServices = accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_GENERIC);
+        for (AccessibilityServiceInfo info : accessibilityServices) {
+            if (info.getId().equals(getPackageName() + "/.QQHongbaoService")) {
+                serviceEnabled = true;
+                break;
+            }
+        }
+        if (serviceEnabled) {
+            switchPlugin.setText("关闭QQ抢红包");
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            switchPlugin.setText("开启QQ抢红包");
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    /**
+     * 打开辅助服务的设置
+     */
+    private void openAccessibilityServiceSettings() {
+        try {
+            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(intent);
+            Toast.makeText(getApplicationContext(), "正在打开【开启服务】设置", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
